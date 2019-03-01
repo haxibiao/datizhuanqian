c4e0943a1e3d095cc0889ad3e0d3a8319d17c3c8
xuan <gaoxuan@haxibiao.com>
2019-03-01 16:59:27 +0800

    UI修复

--- src/screens/profile/settings/AboutScreen.js
+++ src/screens/profile/settings/AboutScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, TouchableOpacity, Text, Image, ScrollView } from 'react-native';
-import { Header } from '../../../components';
+import { Screen } from '../../../components';
 import { Colors, Config, Divice } from '../../../constants';
 
 import { connect } from 'react-redux';
@@ -16,61 +16,69 @@ class AboutScreen extends Component {
 	render() {
 		const { counts } = this.state;
 		return (
-			<ScrollView>
-				<Header />
-				<View style={styles.container}>
-					<View style={{ alignItems: 'center', marginTop: 15 }}>
-						<Image
-							source={require('../../../../assets/images/logo.png')}
-							style={{ width: Divice.width / 4, height: Divice.width / 4 }}
-						/>
-						<Text style={{ color: Colors.black, fontSize: 15, margin: 20 }}>
-							答题赚钱 {Config.AppVersion}
-						</Text>
-					</View>
-					<View style={{ marginTop: 30 }}>
-						<View style={{ paddingHorizontal: 20 }}>
-							<Text style={{ fontSize: 15, color: Colors.black }}>关于答题</Text>
-							<Text
-								style={{
-									fontSize: 13,
-									color: Colors.grey,
-									marginTop: 15,
-									lineHeight: 18,
-									fontWeight: '300'
-								}}
-							>
-								答题赚钱是一款手机休闲益智答题软件,有地理，英文，历史，科学，世界趣闻等知识分类。
-								答题题目将不断更新，让您随时学到新的知识。成功答题的您还能获得收益哦！在等朋友,等公交,等吃饭或其他碎片时间。
-								玩答题赚钱学知识拿金钱，是您killtime的最佳搭档。如果你觉得你掌握的知识够全面就快来答题赚钱吧，各国趣味知识，涵盖天文、地理、历史科学应有尽有。
-								只要你能答对就能赚取相应报酬，快来答题赚钱试试身手吧。
+			<Screen>
+				<ScrollView style={{ flex: 1 }}>
+					<View style={styles.container}>
+						<View style={{ alignItems: 'center', marginTop: 15 }}>
+							<Image
+								source={require('../../../../assets/images/logo.png')}
+								style={{ width: Divice.width / 4, height: Divice.width / 4 }}
+							/>
+							<Text style={{ color: Colors.black, fontSize: 15, margin: 20 }}>
+								答题赚钱 {Config.AppVersion}
 							</Text>
 						</View>
-					</View>
-					<View style={{ marginTop: 30 }}>
-						<View style={{ paddingHorizontal: 20 }}>
-							<Text style={{ fontSize: 15, color: Colors.black }}>联系我们</Text>
-							{/*<Text style={{ fontSize: 13, color: Colors.grey, marginTop: 15 }}>QQ交流群: 4337413</Text>*/}
-							<Text style={{ fontSize: 13, color: Colors.grey, marginTop: 10 }}>
-								官网地址： datizhuanqian.com datizhuanqian.cn
-							</Text>
-							<Text style={{ fontSize: 13, color: Colors.grey, marginTop: 10 }}>
-								商务合作： db@datizhuanqian.com
-							</Text>
-							<Text style={{ fontSize: 13, color: Colors.grey, marginTop: 10 }}>
-								官方邮箱： dtzq@haxibiao.com
-							</Text>
-							<Text style={{ fontSize: 13, color: Colors.grey, marginTop: 10 }}>
-								官方QQ群： 735220029
-							</Text>
+						<View style={{ marginTop: 30 }}>
+							<View style={{ paddingHorizontal: 20 }}>
+								<Text style={{ fontSize: 15, color: Colors.black }}>关于答题</Text>
+								<Text
+									style={{
+										fontSize: 13,
+										color: Colors.grey,
+										marginTop: 15,
+										lineHeight: 18,
+										fontWeight: '300'
+									}}
+								>
+									答题赚钱是一款手机休闲益智答题软件,有地理，英文，历史，科学，世界趣闻等知识分类。
+									答题题目将不断更新，让您随时学到新的知识。成功答题的您还能获得收益哦！在等朋友,等公交,等吃饭或其他碎片时间。
+									玩答题赚钱学知识拿金钱，是您killtime的最佳搭档。如果你觉得你掌握的知识够全面就快来答题赚钱吧，各国趣味知识，涵盖天文、地理、历史科学应有尽有。
+									只要你能答对就能赚取相应报酬，快来答题赚钱试试身手吧。
+								</Text>
+							</View>
+						</View>
+						<View style={{ marginTop: 30 }}>
+							<View style={{ paddingHorizontal: 20 }}>
+								<Text style={{ fontSize: 15, color: Colors.black }}>联系我们</Text>
+								{/*<Text style={{ fontSize: 13, color: Colors.grey, marginTop: 15 }}>QQ交流群: 4337413</Text>*/}
+								<Text style={{ fontSize: 13, color: Colors.grey, marginTop: 10 }}>
+									官网地址： datizhuanqian.com datizhuanqian.cn
+								</Text>
+								<Text style={{ fontSize: 13, color: Colors.grey, marginTop: 10 }}>
+									商务合作： db@datizhuanqian.com
+								</Text>
+								<Text style={{ fontSize: 13, color: Colors.grey, marginTop: 10 }}>
+									官方邮箱： dtzq@haxibiao.com
+								</Text>
+								<Text style={{ fontSize: 13, color: Colors.grey, marginTop: 10 }}>
+									官方QQ群： 735220029
+								</Text>
+							</View>
 						</View>
 					</View>
-				</View>
-				<View style={{ backgroundColor: Colors.lightGray, paddingVertical: 15, alignItems: 'center' }}>
-					<Text>哈希表网络科技(深圳)有限公司</Text>
-					<Text>www.datizhuanqian.com</Text>
-				</View>
-			</ScrollView>
+					<View
+						style={{
+							backgroundColor: Colors.lightGray,
+							marginTop: 30,
+							paddingVertical: 15,
+							alignItems: 'center'
+						}}
+					>
+						<Text>哈希表网络科技(深圳)有限公司</Text>
+						<Text>www.datizhuanqian.com</Text>
+					</View>
+				</ScrollView>
+			</Screen>
 		);
 	}
 }
--- src/screens/profile/settings/LevelDescriptionScreen.js
+++ src/screens/profile/settings/LevelDescriptionScreen.js
@@ -13,83 +13,84 @@ class LevelDescriptionScreen extends Component {
 	render() {
 		const { counts } = this.state;
 		return (
-			<ScrollView>
-				<Header />
-				<View style={styles.container}>
-					<View style={{ paddingVertical: 15 }}>
-						<View style={{ paddingHorizontal: 20 }}>
-							<Text style={{ fontSize: 15, color: Colors.black }}>Q：头衔是什么？有什么作用？</Text>
-							<Text style={{ fontSize: 13, color: Colors.grey, marginTop: 15, lineHeight: 15 }}>
-								A：用户的头衔是用户身份的象征，头衔会根据用户的等级进行变更。等级系统是用户积极答题活跃度的反馈，当用户的等级上升，精力点上限也会随之上升，并且升级还会有很多道具等其他的小奖励哦。
-							</Text>
+			<Screen>
+				<ScrollView>
+					<View style={styles.container}>
+						<View style={{ paddingVertical: 15 }}>
+							<View style={{ paddingHorizontal: 20 }}>
+								<Text style={{ fontSize: 15, color: Colors.black }}>Q：头衔是什么？有什么作用？</Text>
+								<Text style={{ fontSize: 13, color: Colors.grey, marginTop: 15, lineHeight: 15 }}>
+									A：用户的头衔是用户身份的象征，头衔会根据用户的等级进行变更。等级系统是用户积极答题活跃度的反馈，当用户的等级上升，精力点上限也会随之上升，并且升级还会有很多道具等其他的小奖励哦。
+								</Text>
+							</View>
+						</View>
+						<View style={{ paddingVertical: 15 }}>
+							<View style={{ paddingHorizontal: 20 }}>
+								<Text style={{ fontSize: 15, color: Colors.black }}>Q：如何升级？</Text>
+								<Text style={{ fontSize: 13, color: Colors.grey, marginTop: 15, lineHeight: 15 }}>
+									A：如果你想要升级，就必须达到对应的经验值。经验值就是你答对题目的数量，答对一道题经验值+1。所以想要快快升级，就要提高自己答题的正确率哦。
+								</Text>
+							</View>
+						</View>
+						<View style={{ alignItems: 'center', marginTop: 10, paddingHorizontal: 20 }}>
+							<View style={{ flexDirection: 'row' }}>
+								<Text style={styles.text}>等级</Text>
+								<Text style={styles.text}>头衔</Text>
+								<Text style={styles.text}>经验值</Text>
+								<Text style={styles.text}>精力点上限</Text>
+							</View>
+							<View style={{ flexDirection: 'row' }}>
+								<Text style={styles.text}>8</Text>
+								<Text style={styles.text}>仁者无敌</Text>
+								<Text style={styles.text}>10000</Text>
+								<Text style={styles.text}>350</Text>
+							</View>
+							<View style={{ flexDirection: 'row' }}>
+								<Text style={styles.text}>7</Text>
+								<Text style={styles.text}>仁人名师</Text>
+								<Text style={styles.text}>5000</Text>
+								<Text style={styles.text}>320</Text>
+							</View>
+							<View style={{ flexDirection: 'row' }}>
+								<Text style={styles.text}>6</Text>
+								<Text style={styles.text}>为师有道</Text>
+								<Text style={styles.text}>2000</Text>
+								<Text style={styles.text}>300</Text>
+							</View>
+							<View style={{ flexDirection: 'row' }}>
+								<Text style={styles.text}>5</Text>
+								<Text style={styles.text}>学长师友</Text>
+								<Text style={styles.text}>1000</Text>
+								<Text style={styles.text}>280</Text>
+							</View>
+							<View style={{ flexDirection: 'row' }}>
+								<Text style={styles.text}>4</Text>
+								<Text style={styles.text}>有学而志</Text>
+								<Text style={styles.text}>500</Text>
+								<Text style={styles.text}>250</Text>
+							</View>
+							<View style={{ flexDirection: 'row' }}>
+								<Text style={styles.text}>3</Text>
+								<Text style={styles.text}>游学四方</Text>
+								<Text style={styles.text}>200</Text>
+								<Text style={styles.text}>220</Text>
+							</View>
+							<View style={{ flexDirection: 'row' }}>
+								<Text style={styles.text}>2</Text>
+								<Text style={styles.text}>小有所成</Text>
+								<Text style={styles.text}>50</Text>
+								<Text style={styles.text}>200</Text>
+							</View>
+							<View style={{ flexDirection: 'row' }}>
+								<Text style={styles.text}>1</Text>
+								<Text style={styles.text}>初来乍到</Text>
+								<Text style={styles.text}>0</Text>
+								<Text style={styles.text}>180</Text>
+							</View>
 						</View>
 					</View>
-					<View style={{ paddingVertical: 15 }}>
-						<View style={{ paddingHorizontal: 20 }}>
-							<Text style={{ fontSize: 15, color: Colors.black }}>Q：如何升级？</Text>
-							<Text style={{ fontSize: 13, color: Colors.grey, marginTop: 15, lineHeight: 15 }}>
-								A：如果你想要升级，就必须达到对应的经验值。经验值就是你答对题目的数量，答对一道题经验值+1。所以想要快快升级，就要提高自己答题的正确率哦。
-							</Text>
-						</View>
-					</View>
-					<View style={{ alignItems: 'center', marginTop: 10, paddingHorizontal: 20 }}>
-						<View style={{ flexDirection: 'row' }}>
-							<Text style={styles.text}>等级</Text>
-							<Text style={styles.text}>头衔</Text>
-							<Text style={styles.text}>经验值</Text>
-							<Text style={styles.text}>精力点上限</Text>
-						</View>
-						<View style={{ flexDirection: 'row' }}>
-							<Text style={styles.text}>8</Text>
-							<Text style={styles.text}>仁者无敌</Text>
-							<Text style={styles.text}>10000</Text>
-							<Text style={styles.text}>350</Text>
-						</View>
-						<View style={{ flexDirection: 'row' }}>
-							<Text style={styles.text}>7</Text>
-							<Text style={styles.text}>仁人名师</Text>
-							<Text style={styles.text}>5000</Text>
-							<Text style={styles.text}>320</Text>
-						</View>
-						<View style={{ flexDirection: 'row' }}>
-							<Text style={styles.text}>6</Text>
-							<Text style={styles.text}>为师有道</Text>
-							<Text style={styles.text}>2000</Text>
-							<Text style={styles.text}>300</Text>
-						</View>
-						<View style={{ flexDirection: 'row' }}>
-							<Text style={styles.text}>5</Text>
-							<Text style={styles.text}>学长师友</Text>
-							<Text style={styles.text}>1000</Text>
-							<Text style={styles.text}>280</Text>
-						</View>
-						<View style={{ flexDirection: 'row' }}>
-							<Text style={styles.text}>4</Text>
-							<Text style={styles.text}>有学而志</Text>
-							<Text style={styles.text}>500</Text>
-							<Text style={styles.text}>250</Text>
-						</View>
-						<View style={{ flexDirection: 'row' }}>
-							<Text style={styles.text}>3</Text>
-							<Text style={styles.text}>游学四方</Text>
-							<Text style={styles.text}>200</Text>
-							<Text style={styles.text}>220</Text>
-						</View>
-						<View style={{ flexDirection: 'row' }}>
-							<Text style={styles.text}>2</Text>
-							<Text style={styles.text}>小有所成</Text>
-							<Text style={styles.text}>50</Text>
-							<Text style={styles.text}>200</Text>
-						</View>
-						<View style={{ flexDirection: 'row' }}>
-							<Text style={styles.text}>1</Text>
-							<Text style={styles.text}>初来乍到</Text>
-							<Text style={styles.text}>0</Text>
-							<Text style={styles.text}>180</Text>
-						</View>
-					</View>
-				</View>
-			</ScrollView>
+				</ScrollView>
+			</Screen>
 		);
 	}
 }
