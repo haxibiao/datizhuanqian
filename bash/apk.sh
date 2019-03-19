71bb5d6d7eff2ccdc0042fd55781948d33b5fddf
xuan <gaoxuan@haxibiao.com>
2019-03-19 13:11:04 +0800

    dev包  新6大应用商店打包配置

--- bash/apk.sh
+++ bash/apk.sh
@@ -11,6 +11,24 @@ cd ./android
 ./gradlew assembleRelease 
 
 
+echo "${grn}开始生成dev包 ..."
+cd /data/app/datizhuanqian
+
+cd ./android
+./gradlew assembleDev
+
+echo "开始上传dev包到cdn ..."
+if [ ! -d /data/www/datizhuanqian.com ]; then
+	echo "datizhuanqian.com的源码 !!"
+	exit
+fi 
+
+cd /data/www/datizhuanqian.com
+git pull
+mv /data/app/datizhuanqian/android/app/build/outputs/apk/dev/app-dev.apk /data/app/datizhuanqian/android/app/build/outputs/apk/dev/datizhuanqian-dev.apk
+php artisan upload:cos APK --filePath=/data/app/datizhuanqian/android/app/build/outputs/apk/dev/datizhuanqian-dev.apk --is_prod=true
+
+
 echo "${grn}开始生成内测包 ..."
 cd /data/app/datizhuanqian
 
@@ -44,5 +62,42 @@ cd /data/app/datizhuanqian
 
 cd ./android
 ./gradlew assembleBaidu
+
+echo "${grn}开始生成豌豆荚正式包 ...${end}"
+cd /data/app/datizhuanqian
+
+cd ./android
+./gradlew assembleWandoujia
+
+echo "${grn}开始生成VIVO正式包 ...${end}"
+cd /data/app/datizhuanqian
+
+cd ./android
+./gradlew assembleVivo
+
+echo "${grn}开始生成OPPO正式包 ...${end}"
+cd /data/app/datizhuanqian
+
+cd ./android
+./gradlew assembleOppo
+
+echo "${grn}开始生成华为正式包 ...${end}"
+cd /data/app/datizhuanqian
+
+cd ./android
+./gradlew assembleHuawei
+
+echo "${grn}开始生成魅族正式包 ...${end}"
+cd /data/app/datizhuanqian
+
+cd ./android
+./gradlew assembleMeizu
+
+echo "${grn}开始生成360正式包 ...${end}"
+cd /data/app/datizhuanqian
+
+cd ./android
+./gradlew assembleQihoo
+
 
 #单独生成某个包的话手动去生成比较方便
