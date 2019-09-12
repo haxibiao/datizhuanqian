0bf2be1caacfa776625157f50d1a8fa8c040e38d
xuan <597455096@qq.com>
2019-09-12 15:45:15 +0800

    修复系统维护

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@
  */
 
 import React, { Fragment, Component } from 'react';
-import { StyleSheet, YellowBox, View } from 'react-native';
+import { StyleSheet, YellowBox, View, Image, Text } from 'react-native';
 import { Toast, ErrorBoundary } from 'components';
 import { app, config } from 'store';
 
 import Orientation from 'react-native-orientation';
 import codePush from 'react-native-code-push';
 import { TtAdvert, WeChat } from 'native';
-import { ISIOS, Config } from 'utils';
+import { ISIOS, Config, PxFit, Theme } from 'utils';
 
 import service from 'service';
 import { checkUpdate } from 'common';
@@ -26,6 +26,10 @@ class App extends Component {
     constructor(props) {
         super(props);
         global.TOKEN = null;
+        this.state = {
+            serverMaintenance: false,
+            responseText: null,
+        };
 
         YellowBox.ignoreWarnings([
             'Remote debugger is in a background tab',
@@ -46,9 +50,11 @@ class App extends Component {
             config.saveAdvertConfig(data);
             if (data.enable_splash) {
                 // 开屏广告因为时机问题直接在此判断了
-                TtAdvert.Splash.loadSplashAd();
+                // TtAdvert.Splash.loadSplashAd();
             }
         });
+
+        TtAdvert.Splash.loadSplashAd();
         // 检查更新
         checkUpdate('autoCheck');
         // 检查GQL接口状态
@@ -69,11 +75,34 @@ class App extends Component {
                 if (response.status === 503) {
                     this.setState({ serverMaintenance: response });
                 }
+                return response.text();
+            })
+            .then(responseText => {
+                this.setState({
+                    responseText: responseText,
+                });
             })
             .catch(error => {
                 console.warn('server error', error);
             });
     };
+
+    _showMaintenance() {
+        const { serverMaintenance, responseText } = this.state;
+
+        if (serverMaintenance) {
+            return (
+                <View style={styles.maintenance}>
+                    <Image style={styles.image} source={require('./assets/images/server_maintenance.jpg')} />
+                    <View style={styles.textWrap}>
+                        <Text style={styles.text}>{responseText || '服务器维护中,先休息一会儿吧!'}</Text>
+                    </View>
+                </View>
+            );
+        } else {
+            return null;
+        }
+    }
 
     render() {
         return (
@@ -82,6 +111,7 @@ class App extends Component {
                     <ErrorBoundary>
                         <Apollo checkServer={this.checkServer} />
                     </ErrorBoundary>
+                    {this._showMaintenance()}
                     <Toast ref={ref => (this.toast = ref)} />
                 </View>
             </Fragment>
@@ -93,6 +123,38 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#FFF',
         flex: 1,
+    },
+    maintenance: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: '#fff',
+    },
+    image: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        width: null,
+        height: null,
+    },
+    textWrap: {
+        position: 'absolute',
+        top: '65%',
+        left: 0,
+        right: 0,
+        paddingHorizontal: PxFit(20),
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: PxFit(15),
+        lineHeight: PxFit(18),
+        color: Theme.subTextColor,
     },
 });
 
