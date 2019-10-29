adbbe90bd4fdf046d38a0fe8c42d534a161be1dd
xuan <597455096@qq.com>
2019-10-29 16:48:05 +0800

    DZ-930 优化提示失败信息提示

--- src/screens/withdraw/components/WithdrawLogItem.js
+++ src/screens/withdraw/components/WithdrawLogItem.js
@@ -5,59 +5,61 @@
 'use strict';
 import React, { Component } from 'react';
 import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
-import { Theme, PxFit } from '../../../utils';
+import { Theme, PxFit, SCREEN_WIDTH } from '../../../utils';
 
 function WithdrawLogItem(props) {
-	const { style, navigation, item } = props;
-	let statusText, color;
-	switch (item.status) {
-		case -1:
-			statusText = '提现失败';
-			color = Theme.errorColor;
-			break;
-		case 1:
-			statusText = '提现成功';
-			color = Theme.weixin;
-			break;
-		case 0:
-			statusText = '待处理';
-			color = Theme.correctColor;
-			break;
-	}
-	return (
-		<TouchableOpacity
-			style={[styles.item, style]}
-			activeOpacity={0.7}
-			disabled={item.status == 0}
-			onPress={() =>
-				navigation.navigate('withdrawLogDetails', {
-					withdraw_id: item.id
-				})
-			}
-		>
-			<View>
-				<Text style={{ fontSize: PxFit(15), color: Theme.defaultTextColor }}>{statusText}</Text>
-				<Text style={{ fontSize: PxFit(12), color: Theme.subTextColor, paddingTop: PxFit(10) }}>
-					{item.created_at}
-				</Text>
-			</View>
-			<View>
-				<Text style={{ fontSize: PxFit(20), color }}>￥{item.amount.toFixed(0)}.00</Text>
-			</View>
-		</TouchableOpacity>
-	);
+    const { style, navigation, item } = props;
+    let statusText, color;
+    switch (item.status) {
+        case -1:
+            statusText = '提现失败';
+            color = Theme.errorColor;
+            break;
+        case 1:
+            statusText = '提现成功';
+            color = Theme.weixin;
+            break;
+        case 0:
+            statusText = '待处理';
+            color = Theme.correctColor;
+            break;
+    }
+    return (
+        <TouchableOpacity
+            style={[styles.item, style]}
+            activeOpacity={0.7}
+            disabled={item.status == 0}
+            onPress={() =>
+                navigation.navigate('withdrawLogDetails', {
+                    withdraw_id: item.id,
+                })
+            }>
+            <View style={{ width: (SCREEN_WIDTH * 5) / 7 }}>
+                <Text style={{ fontSize: PxFit(15), color: Theme.defaultTextColor }}>
+                    {statusText}
+                    <Text style={{ fontSize: 15, color: Theme.themeRed }}>{item.remark && `   (${item.remark})`}</Text>
+                </Text>
+                <Text style={{ fontSize: PxFit(12), color: Theme.subTextColor, paddingTop: PxFit(10) }}>
+                    {item.created_at}
+                </Text>
+            </View>
+            <View>
+                <Text style={{ fontSize: PxFit(20), color }}>￥{item.amount.toFixed(0)}.00</Text>
+            </View>
+        </TouchableOpacity>
+    );
 }
 
 const styles = StyleSheet.create({
-	item: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'space-between',
-		paddingVertical: 10,
-		borderBottomColor: Theme.lightBorder,
-		borderBottomWidth: 0.5,
-		paddingHorizontal: 15
-	}
+    item: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingVertical: 10,
+        borderBottomColor: Theme.lightBorder,
+        borderBottomWidth: 0.5,
+        paddingHorizontal: 15,
+    },
 });
 
 export default WithdrawLogItem;
