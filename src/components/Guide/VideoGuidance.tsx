1a1bf1f87c32824fbfa13bf7e6c286b71a938692
xuan <597455096@qq.com>
2019-11-04 14:37:56 +0800

    DZ-902 完善刷视频引导

--- src/components/Guide/VideoGuidance.tsx
+++ src/components/Guide/VideoGuidance.tsx
@@ -1,23 +1,60 @@
 /*
  * @flow
- * created by wyk made in 2019-09-10 14:32:24
+ * created by wyk made in 2019-09-09 11:10:28
  */
 import React, { useState, useMemo } from 'react';
 import { StyleSheet, Text, View, Image, TouchableWithoutFeedback } from 'react-native';
 import { PxFit, Theme, SCREEN_WIDTH, NAVBAR_HEIGHT, SCREEN_HEIGHT, Tools } from 'utils';
+import { app } from 'store';
+import { ttad } from 'native';
+import { playVideo } from 'common';
 
 function VideoGuidance({ onDismiss }) {
-    return (
-        <View style={styles.flexCenter}>
-            <Image style={styles.answerGuide} source={require('../../assets/images/video_guide.gif')} />
-        </View>
-    );
+    const [step, setStep] = useState(0);
+    const me = useMemo(() => app.me, [app]);
+    const guidesView = useMemo(() => {
+        return [
+            <TouchableWithoutFeedback
+                key={1}
+                onPress={() => {
+                    setStep(1);
+                }}>
+                <View style={styles.flexCenter}>
+                    <Image style={styles.answerGuide} source={require('../../assets/images/video_guide_1.gif')} />
+                </View>
+            </TouchableWithoutFeedback>,
+            <TouchableWithoutFeedback
+                key={2}
+                onPress={() => {
+                    app.changeUserStatus(false);
+                    onDismiss();
+                }}>
+                <View style={styles.flexCenter}>
+                    <TouchableWithoutFeedback
+                        onPress={() => {
+                            app.changeUserStatus(false);
+                            playVideo({ type: 'Task' });
+                            onDismiss();
+                        }}>
+                        <Image
+                            style={styles.stimulateVideo}
+                            source={require('../../assets/images/video_guide_2.png')}
+                        />
+                    </TouchableWithoutFeedback>
+                </View>
+            </TouchableWithoutFeedback>,
+        ];
+    }, []);
+
+    return guidesView[step];
 }
+
+const videoGuideWidth = PxFit(88) * (800 / 252);
+const videoGuideHeight = (videoGuideWidth * 442) / 931;
+const videoGuideBottom = PxFit(340 + Theme.HOME_INDICATOR_HEIGHT);
 
 const WIDTH = SCREEN_WIDTH / 2;
 const HEIGHT = (WIDTH * 1026) / 772;
-const TOP = PxFit(NAVBAR_HEIGHT);
-const RIGHT = WIDTH / 2 - (WIDTH * 140) / 411;
 
 const styles = StyleSheet.create({
     flexCenter: {
@@ -30,6 +67,19 @@ const styles = StyleSheet.create({
     answerGuide: {
         width: WIDTH,
         height: HEIGHT,
+        resizeMode: 'contain',
+    },
+    userReward: {
+        width: (SCREEN_WIDTH * 4) / 5,
+        height: (((SCREEN_WIDTH * 4) / 5) * 640) / 519,
+        resizeMode: 'contain',
+    },
+    stimulateVideo: {
+        position: 'absolute',
+        bottom: videoGuideBottom,
+        right: PxFit(Theme.itemSpace),
+        width: videoGuideWidth,
+        height: videoGuideHeight,
         resizeMode: 'contain',
     },
 });
--- src/screens/withdraw/components/RuleDescription.js
+++ src/screens/withdraw/components/RuleDescription.js
@@ -10,55 +10,56 @@ import { Config, Theme, PxFit, SCREEN_WIDTH } from '../../../utils';
 import { Overlay } from 'teaset';
 
 class RuleDescription extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-	render() {
-		return (
-			<View
-				style={{
-					width: SCREEN_WIDTH - PxFit(70),
-					paddingHorizontal: PxFit(25),
-					paddingVertical: PxFit(20),
-					borderRadius: PxFit(15),
-					backgroundColor: '#fff'
-				}}
-			>
-				<Text style={{ color: Theme.defaultTextColor, fontSize: PxFit(18), textAlign: 'center' }}>
-					规则说明
-				</Text>
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-				<View style={{ marginTop: PxFit(5) }}>
-					<Text style={styles.text}>1.只有当您绑定支付宝账号之后，才能开始提现。</Text>
-					<Text style={styles.text}>
-						2.提现金额分为1元、3元、5元、10元四档，每次提现时会扣除智慧点，剩余智慧点可以在下次满足最低提现额度时申请提现。
-					</Text>
-					<Text style={styles.text}>
-						3.一个账号只能绑定一个支付宝提现，使用多个账号绑定同一支付宝，提现系统将判定涉嫌恶意刷取智慧点，
-						{Config.AppName}官方将做限制提现处理,同时清空所有智慧点。
-					</Text>
-					<Text style={styles.text}>4.提现24小时内到账(如遇提现高峰，提现到账时间会延长)。</Text>
-				</View>
-				<Button title={'知道了'} onPress={() => this.props.hide()} style={styles.buttonText} />
-			</View>
-		);
-	}
+    render() {
+        return (
+            <View
+                style={{
+                    width: SCREEN_WIDTH - PxFit(70),
+                    paddingHorizontal: PxFit(25),
+                    paddingVertical: PxFit(20),
+                    borderRadius: PxFit(15),
+                    backgroundColor: '#fff',
+                }}>
+                <Text style={{ color: Theme.defaultTextColor, fontSize: PxFit(18), textAlign: 'center' }}>
+                    规则说明
+                </Text>
+
+                <View style={{ marginTop: PxFit(5) }}>
+                    <Text style={styles.text}>1.只有当您绑定支付宝账号之后，才能开始提现。</Text>
+                    <Text style={styles.text}>
+                        2.提现金额分为1元、3元、5元、10元四档，每次提现时会扣除智慧点，剩余智慧点可以在下次满足最低提现额度时申请提现。
+                    </Text>
+                    <Text style={styles.text}>3.每日只能进行一次提现。</Text>
+                    <Text style={styles.text}>
+                        4.一个账号只能绑定一个支付宝提现，使用多个账号绑定同一支付宝，提现系统将判定涉嫌恶意刷取智慧点，
+                        {Config.AppName}官方将做限制提现处理,同时清空所有智慧点。
+                    </Text>
+                    <Text style={styles.text}>4.提现24小时内到账(如遇提现高峰，提现到账时间会延长)。</Text>
+                </View>
+                <Button title={'知道了'} onPress={() => this.props.hide()} style={styles.buttonText} />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-	text: {
-		paddingVertical: PxFit(2),
-		lineHeight: PxFit(18),
-		fontSize: PxFit(12),
-		color: Theme.subTextColor
-	},
-	buttonText: {
-		height: PxFit(38),
-		borderRadius: PxFit(19),
-		marginTop: PxFit(10),
-		backgroundColor: Theme.primaryColor
-	}
+    text: {
+        paddingVertical: PxFit(2),
+        lineHeight: PxFit(18),
+        fontSize: PxFit(12),
+        color: Theme.subTextColor,
+    },
+    buttonText: {
+        height: PxFit(38),
+        borderRadius: PxFit(19),
+        marginTop: PxFit(10),
+        backgroundColor: Theme.primaryColor,
+    },
 });
 
 export default RuleDescription;
