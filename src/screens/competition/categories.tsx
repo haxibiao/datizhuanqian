c9f32d1b3e2b70ef2c7bc602edac657bad0ae0c1
wyk <wangyukun@haxibiao.com>
2019-11-07 14:52:34 +0800

    DZ-182 选择答题分类

--- src/screens/competition/categories.tsx
+++ src/screens/competition/categories.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useMemo, useRef, useEffect, useCallback } from 'react';
+import { StyleSheet, FlatList, View, Text, Image, ImageBackground, TouchableOpacity } from 'react-native';
+import { Theme, SCREEN_WIDTH, Tools, PxFit } from 'utils';
+import { useNavigation } from 'react-navigation-hooks';
+import { GQL, useQuery, useApolloClient } from 'apollo';
+import { syncGetter, exceptionCapture, GetSpecifyDate } from 'common';
+import { PageContainer } from 'components';
+import { observer, app } from 'store';
 
-const categories = () => {
+export default observer(props => {
+    const { me } = app;
+    const navigation = useNavigation();
+    const client = useApolloClient();
+
+    const { data, refetch } = useQuery(GQL.CategoriesQuery, {
+        variables: {
+            limit: 100,
+        },
+    });
+
+    const categories = syncGetter('categories', data);
+
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <PageContainer style={styles.container}>
+            <View />
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                data={categories}
+                keyExtractor={(item, index) => (item.id ? item.id.toString() + Date.now() : index.toString())}
+                renderItem={({ item, index }) => <View category={item} navigation={navigation} />}
+                onEndReachedThreshold={0.1}
+                onEndReached={() => null}
+                ListHeaderComponent={() => <View />}
+                ListFooterComponent={() => <View finished={this.state.finished} />}
+            />
+        </PageContainer>
     );
-};
+});
 
-export default categories;
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        flex: 1,
+    },
+});
