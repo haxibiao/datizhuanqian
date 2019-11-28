3e8dff0450aef773b03828dd1d9e50c0688cc74f
ivan <ivanzhang001@gmail.com>
2019-11-28 15:38:08 +0800

    bash bash/pgyer.sh upload才真的上传ipa到蒲公英

--- bash/pgyer.sh
+++ bash/pgyer.sh
@@ -9,28 +9,30 @@ cyn=$'\e[1;36m'
 end=$'\e[0m'
 
 app="datizhuanqian"
-
+upload="no"
 if [ ! -z $1 ]; then
-    app=$1
+    upload="yes"
 fi
 
 cd /data/app/$app/
 sudo chmod -R 777 .
-
 [ ! -d /data/build/app ] && mkdir -p /data/build/app
 
+if [ $upload != "yes" ]; then
 # echo -e "\n${grn}clean $app ...${end}"
 # xcodebuild clean -UseModernBuildSystem=YES -workspace /data/app/$app/ios/$app.xcworkspace -scheme "$app"
 
-# echo -e "\n${grn}archive $app ...${end}"
-# xcodebuild archive -UseModernBuildSystem=YES -workspace "/data/app/$app/ios/$app.xcworkspace" -scheme "$app" -archivePath "/data/build/app/$app"
+echo -e "\n${mag}archive $app ...${end}"
+xcodebuild archive -UseModernBuildSystem=YES -workspace "/data/app/$app/ios/$app.xcworkspace" -scheme "$app" -archivePath "/data/build/app/$app"
 
 echo -e "\n${grn}export ipa $app ...${end}"
 xcodebuild -UseModernBuildSystem=YES -exportArchive -archivePath  /data/build/app/$app.xcarchive  -exportPath  /data/build/app/$app  -exportOptionsPlist /data/app/$app/bash/etc/exportOptions.plist
 
 echo -e "\n${grn}最后一步，复制为 /data/build/app/$app.ipa 方便后面上传 ...${end}"
 /bin/cp -f /data/build/app/$app/$app.ipa /data/build/app/$app.ipa
+fi 
 
-
-echo "上传 $app.ipa ..."
+if [ $upload = "yes" ]; then
+echo -e "\n${yel}上传 $app.ipa 到蒲公英...${end}"
 curl -F "file=@/data/build/app/$app.ipa" -F "uKey=886ce154bbec732cfb657fd24968fb75" -F "_api_key=7ddece27311d35a058f29e66a8ddccd6" https://qiniu-storage.pgyer.com/apiv1/app/upload
+fi
