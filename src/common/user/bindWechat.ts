e0cabc8a4bab17acdda7a2ecc3c9180cd83229f8
xuan <gaoxuan@haxibiao.com>
2019-12-03 11:58:03 +0800

    DZ-1046 修复未登录绑定微信卡住

--- src/common/user/bindWechat.ts
+++ src/common/user/bindWechat.ts
@@ -12,9 +12,13 @@ export function bindWechat(props: Props) {
     WeChat.isSupported()
         .then((isSupported: any) => {
             if (isSupported) {
-                WeChat.wechatLogin().then((code: any) => {
-                    bindWx(code, props);
-                });
+                WeChat.wechatLogin()
+                    .then((code: any) => {
+                        bindWx(code, props);
+                    })
+                    .catch(err => {
+                        Toast.show({ content: '微信身份信息获取失败，请检查微信是否登录' });
+                    });
             } else {
                 Toast.show({ content: '未安装微信或当前微信版本较低' });
             }
