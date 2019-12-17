eecf5df3eb5c3ab5777be6df3d547c96dc147819
xuan <gaoxuan@haxibiao.com>
2019-12-17 10:19:19 +0800

    DZ-1107  增加根据版本号获取广告开关配置

--- src/service/fetch/enableAdvert.js
+++ src/service/fetch/enableAdvert.js
@@ -1,11 +1,26 @@
+import { Platform } from 'react-native';
 import { Config } from 'utils';
+
+const deviceHeaders = {};
+
+deviceHeaders.os = Platform.OS; // 操作系统
+deviceHeaders.build = Config.Build; // 手动修改的build版本号
+deviceHeaders.referrer = Config.AppStore; // 应用商店来源
+deviceHeaders.version = Config.Version; // 手动修改的App版本号
+deviceHeaders.appid = Config.PackageName; // 手动修改的包名
+deviceHeaders.package = Config.PackageName; // 手动修改的包名
 
 //获取广告开启配置
 export default function(callback: Promise) {
     Promise.race([
-        fetch(Config.ServerRoot + '/api/app-ad-manage?' + Date.now()),
+        fetch(Config.ServerRoot + '/api/app-ad-manage?' + Date.now(), {
+            method: 'POST',
+            headers: {
+                ...deviceHeaders,
+            },
+        }),
         new Promise((resolve, reject) => {
-            setTimeout(() => reject(new Error('request timeout')), 800);
+            setTimeout(() => reject(new Error('request timeout')), 1000);
         }),
     ])
         .then(response => response.json())
